3b0bccdf7e98fed8b0d20b209e288495fd73f292
SerjeiMikailov <felipemaders8@gmail.com>
2023-07-08 00:35:35 -0300

    now organized

--- src/App.h
+++ src/App.h
@@ -7,14 +7,26 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <limits.h>
+#include <sys/resource.h>
 
 #define MAX_COMMAND_LENGHT 100
 #define MAX_ARGS 20
 #define MAX_PATH 50000
 
+// commands
+void execute_command(char **args);
+
+// reader functions
+void read_command(char *command);
+void parse_command(char *command, char **args);
+
+// system level executions
+void executeSystemCommand(const char* command);
+
+// misc
+void print_memory_usage();
 void remove_directory(const char *path);
 char* get_user();
 void clear();
 char* working_dir();
 void shellb();
-void executeSystemCommand(const char* command);
