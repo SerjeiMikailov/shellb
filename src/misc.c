d96b79f53529460818dd6a34e2d7f6fad3d85a58
SerjeiMikailov <felipemaders8@gmail.com>
2023-07-08 00:36:57 -0300

    memory usage func

--- src/misc.c
+++ src/misc.c
@@ -23,6 +23,16 @@ char* working_dir() {
     }
 }
 
+void print_memory_usage() {
+    struct rusage usage;
+    if (getrusage(RUSAGE_SELF, &usage) == 0) {
+        long ram_usage = usage.ru_maxrss;
+        printf("Memory usage: %ld KB\n", ram_usage);
+    } else {
+        puts("Failed to retrieve memory usage");
+    }
+}
+
 void shellb() {
     printf("\033[1;36m----------------\033[0m\n");
     printf("\033[1;96m-    ShellB    -\033[0m\n");
