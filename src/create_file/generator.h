3e709cad1b57cf9073e6a98eba7165a07d9bdf36
SerjeiMikailov <felipemaders8@gmail.com>
2023-07-27 22:00:42 -0300

    create file command

---
+++ src/create_file/generator.h
@@ -0,0 +1,16 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void createCFile(const char* filename, const char* content);
+void te(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+ // void createCFile(const char* filename, const char* content);
