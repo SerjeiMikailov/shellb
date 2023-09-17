fcb2c817ef9b0adbe36e90ef07a2828a341d0489
SerjeiMikailov <felipemaders8@gmail.com>
2023-09-17 17:47:24 -0300

    g

--- installer/new_installer.sh
+++ installer/new_installer.sh
@@ -1,75 +1,32 @@
-#!/bin/bash
 
 git clone -b unstablev2 https://github.com/SerjeiMikailov/shellb.git
 
 cd shellb
-
 cd $OLDPWD
 
-mv shellb $HOME
+mkdir $HOME/shellbsrc
+
+mv shellb $HOME/shellbsrc
 
 cd $HOME
-
-cd shellb
+cd shellbsrc
+cd shellb 
 
 sh client_compiler.sh
 
-cd build 
+cd build
 
-sudo cp shellb /usr/local/bin/
-
+sudo cp shellb /usr/local/bin
 sudo chmod +x /usr/local/bin/shellb
 
-hash -r
-
-echo "Path: Success"
-
-cd $HOME
-
-mkdir shellbsrc
-
-cd $OLDPWD
-
 cd ..
+cd installer
 
-cd script
+sudo cp client_updater /usr/local/bin
+sudo chmod +x /usr/local/bin/client_updater
 
-cp script.cpp  $HOME/shellbsrc
-
-cp script.hpp  $HOME/shellbsrc
-
-cd .. 
-
-cd script_area
-
-cp shellb_script.cpp $HOME/shellbsrc
-
-cd compiler
-
-cp run.sh $HOME/shellbsrc
-
-cd ../..
-
-cd src 
-
-cp App.h $HOME/shellbsrc
-cp misc.c $HOME/shellbsrc
-cp generator.h $HOME/shellbsrc
-
-cd ..
-
-cd installer 
-
-cp new_installer.sh $HOME/shellbsrc
-
-sudo mv uninstall.sh /usr/local/bin
+sudo cp uninstall.sh /usr/local/bin
 sudo chmod +x /usr/local/bin/uninstall.sh
 
-sudo mv client_updater.sh /usr/local/bin
-sudo chmod +x /usr/local/bin/client_updater.sh
-
 hash -r
-
-echo "Installation complete!"
-
 echo "Now type shellb in your terminal"
