ec4a512c815507d36c99615be17d55427602d5e4
SerjeiMikailov <felipemaders8@gmail.com>
2023-09-17 17:56:36 -0300

    g

--- installer/client_updater.sh
+++ installer/client_updater.sh
@@ -10,14 +10,23 @@ sudo rm -rf shellb
 
 cd $HOME
 
+mkdir updatesb
+
+cd updatesb
+
 git clone -b unstablev2 https://github.com/SerjeiMikailov/shellb.git
 
 cd shellb
 
 cd installer 
 
+cp new_installer.sh $HOME/updatesb
+
+cd $HOME
+cd updatesb
+
 sh new_installer.sh
 
-cd ../.. 
+cd $HOME
 
-rm -rf shellb
+rm -rf updatesb
