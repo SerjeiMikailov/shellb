ebc8adc3c1e750d6a932b9ba2b43d34c374bc2cf
SerjeiMikailov <felipemaders8@gmail.com>
2023-09-17 22:41:04 -0300

    g

--- config/config_exec.c
+++ config/config_exec.c
@@ -5,13 +5,13 @@ void editor_select()
 {
   if(editor == 2)
   {
-    system("vim $HOME/projects/shellb/config/config.c");
+    system("vim $HOME/shellbsrc/shellb/config/config.c");
   } else if(editor == 3)
   {
-    system("code $HOME/projects/shellb/config/config.c");  
+    system("code $HOME/shellbsrc/shellb/config/config.c");  
   } else if(editor == 1)
   {
-    system("nano $HOME/projects/shellb/config/config.c");  
+    system("nano $HOME/shellbsrc/shellb/config/config.c");  
   }
 
 }
